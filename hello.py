8bed1e827d891b6a5781421cbf69c0de483bb6c2
Jeremiah Smith <TheButtonBreaker@outlook.com>
2017-09-01 19:57:55 +0000

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,10 @@
+print(" Hello World :)")
+print("*********************************")
+print("*    JJJ   RRRR     SSSSS     *")
+print("*    JJJ   R  RR    SS        *")
+print("*    JJJ   RRRR     SSSSS     *")
+print("*    JJJ   R   RR      SS     *")
+print("*  JJJJJ   R    RRR SSSSS     *")
+
+
+
