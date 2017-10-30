82ac1ffabb0d5554a4dc8638756377e0b0baca57
Jeremiah Smith <TheButtonBreaker@outlook.com>
2017-10-30 12:08:45 -0700

    Add files via upload

--- Python/sfi_print.py
+++ Python/sfi_print.py
@@ -1,9 +1,11 @@
 def string():
-	c = ('This is a string')
+	print ("Hello")
+def float():
+	print (14.5)
+def integer():
+	print (56)
+string()
+float()
+integer()
 
-def main():
-	c = string
-	print(c)
-
-main()
 
